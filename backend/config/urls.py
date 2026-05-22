318a7c17351058bd7e1d367862b53d660e2fc41e
had-jer-lab <hadjertouala@gmail.com>
2026-05-22 19:17:40 +0200

    fix: package.json syntax error and urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,3 @@
-# ══════════════════════════════════════════
-# config/urls.py
-# ══════════════════════════════════════════
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -8,11 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin-panel/', admin.site.urls),
-    path('api/auth/',    include('accounts.urls')),
-    path('api/patients/',include('patients.urls')),
-    path('api/statistic/', include('statistic.urls')),
-    path('api/rcp/',     include('rcp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # ✅ في الأسفل
-    path('api/', include('patients.urls')),
 
+    path('api/auth/', include('accounts.urls')),
+    path('api/patients/', include('patients.urls')),
+    path('api/rcp/', include('rcp.urls')),
+    path('api/statistic/', include('statistic.urls')),
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
